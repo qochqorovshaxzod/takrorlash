b6e0bee1f9f78e4fbaad531274090c5837d9ce6e
shaxzod09 <shaxzodqochqorov477@gmail.com>
2026-08-16 22:50:11 -0700

    add kalkulator.py

---
+++ kalkulyator.py
@@ -0,0 +1,43 @@
+# misol1 = int(input('birinchi soni kiriting: '))
+# misol2 = int(input('ikkinchi soni kiriting: '))
+# amal = input('amal kiriting: ')
+
+# if amal == '+':
+#     print('son1 + son2')
+# elif amal == '-':
+#     print('son1 - son2')
+# elif amal == '*':
+#     print('son1 * son2')
+# elif amal == '/':
+#     print('son1 / son2')
+# else:
+#    print('son1 == son2')
+
+
+def qoshish(a,b):
+    print(a+b)
+
+def ayirish(a,b):
+    print(a-b)
+
+def kop(a,b):
+    print(a*b)
+
+def boluv(a,b):
+    print(a/b)
+
+
+son1 = int(input('birichi soni kiriting: '))
+son2 = int(input('ikkinchi soni kiriting: '))
+amal = input('amal kiriting: ')
+
+if amal == "+":
+    print('son1 + son2')
+elif amal == '-':
+    print('son1 - son2')
+elif amal == '*':
+    print('son1 * son2')
+elif amal == '/':
+    print('son1 / son2')
+else:
+    print('notogri amal kiritingiz')
